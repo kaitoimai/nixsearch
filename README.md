2023eb204cf356c3bbb00f7a0cb58354b7ad5519
kaitoimai <kaito87imai@gmail.com>
2026-06-21 00:50:36 +0900

    Initial nixsearch flake

---
+++ README.md
@@ -0,0 +1,62 @@
+# nixsearch
+
+`nixsearch` searches nixpkgs and shows whether matching packages are available
+for Apple Silicon Darwin.
+
+## Usage
+
+Run directly from GitHub:
+
+```sh
+nix run github:kaitoimai/nixsearch -- ripgrep
+```
+
+Install into a profile:
+
+```sh
+nix profile install github:kaitoimai/nixsearch
+```
+
+Then run:
+
+```sh
+nixsearch ripgrep
+```
+
+## nix-darwin
+
+Add this repository as a flake input:
+
+```nix
+inputs.nixsearch.url = "github:kaitoimai/nixsearch";
+```
+
+Pass it to Home Manager:
+
+```nix
+home-manager.extraSpecialArgs = {
+  inherit nixsearch;
+};
+```
+
+Install the package:
+
+```nix
+{
+  pkgs,
+  nixsearch,
+  ...
+}: {
+  home.packages = [
+    nixsearch.packages.${pkgs.system}.default
+  ];
+}
+```
+
+## Development
+
+```sh
+nix develop
+nix fmt
+nix flake check
+```
---
+++ flake.nix
@@ -0,0 +1,90 @@
+{
+  description = "Search nixpkgs packages and show Darwin availability";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      default = pkgs.writeShellApplication {
+        name = "nixsearch";
+        runtimeInputs = [
+          pkgs.jq
+        ];
+        text = builtins.readFile ./bin/nixsearch;
+      };
+    });
+
+    apps = forAllSystems (system: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/nixsearch";
+        meta.description = "Search nixpkgs packages and show Darwin availability";
+      };
+    });
+
+    checks = forAllSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      smoke =
+        pkgs.runCommand "nixsearch-smoke" {
+          nativeBuildInputs = [
+            self.packages.${system}.default
+          ];
+        } ''
+          nixsearch --help >/dev/null
+          touch "$out"
+        '';
+    });
+
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.alejandra
+          pkgs.jq
+          pkgs.shellcheck
+        ];
+      };
+    });
+
+    formatter = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      pkgs.writeShellApplication {
+        name = "format-nixsearch";
+        runtimeInputs = [
+          pkgs.alejandra
+        ];
+        text = ''
+          if [ "$#" -eq 0 ]; then
+            exec alejandra flake.nix
+          fi
+
+          exec alejandra "$@"
+        '';
+      });
+  };
+}
